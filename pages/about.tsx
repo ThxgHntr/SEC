1435eb9f1aba99c14b5d136082c0ba8eb565bdcd
Huy <trinhdamhuy@gmail.com>
2023-11-08 13:02:11 +0700

    c

--- pages/about.tsx
+++ pages/about.tsx
@@ -61,8 +61,6 @@ const AboutPage = () => {
       <hr className="max-w-4xl h-0.5 mx-auto my-4 bg-xanh-le border-0 rounded"></hr>
       <div className="flex flex-col items-center gap-2 m-3">
         <QA />
-        <QA />
-        <QA />
       </div>
 
       <p className="text-4xl font-bold text-center my-5 pt-10">
