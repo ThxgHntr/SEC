1b09a36a4aab3c27445af32524dbc4350662e699
Huy <trinhdamhuy@gmail.com>
2023-11-22 12:23:33 +0700

    QA

--- components/QA.tsx
+++ components/QA.tsx
@@ -1,15 +1,21 @@
 const QA = () => {
   return (
-    <div className="w-full rounded-2xl shadow-xl my-1 flex flex-col gap-4 p-4 max-w-6xl text-xl">
-      <table>
+    <div className="w-full my-1 flex flex-col p-4 max-w-6xl text-xl">
+      <table className="table-auto border-collapse border-spacing-4">
+        <thead>
+          <tr>
+            <th className="p-4 border border-slate-700">STT</th>
+            <th className="p-4 border border-slate-700">Q</th>
+            <th className="p-4 border border-slate-700">A</th>
+          </tr>
+        </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>Q.
+            <td className="p-4 border border-slate-700">1</td>
+            <td className="p-4 border border-slate-700">
               Nếu chúng ta phát triển được một ngôi làng, thì chúng ta cũng cũng sẽ tìm ra cách phát triển những ngôi làng khác. Do vậy tôi thấy dự án này phát triển một ngôi làng này rất có tầm nhìn và bền vững. Tôi muốn hiểu hơn về các chương trình cụ thể, từ đó tôi có thể sắp xếp để tham gia một vài chương trình nào đó.
             </td>
-            <td>
-              A.
+            <td className="p-4 border border-slate-700">
               Cảm ơn ý kiến đóng góp chân thành và tầm nhìn tuyệt vời của bạn.
               Tất cả chúng ta đang rất hào hứng thực hiện dự án này. Chúng ta tin tưởng mãnh liệt vào sự nỗ lực, hợp tác và tình yêu mà chúng ta dành con người và vùng đất này.
               Dự án có nhiều chương trình. Đó là các chương trình đào tạo bằng thực tế, phù hợp với tiềm năng con người và tự nhiên của vùng. Đối với làng PÀ-căng mới thì nông nghiệp, du lịch dịch vụ xanh là rất tiềm năng. Do vậy, các chương trình đào tạo sẽ tập trung vào:
@@ -20,11 +26,6 @@ const QA = () => {
               Phát triển kỹ năng giao tiếp và ngoại ngữ
               Hãy gọi 0962 414 125 hoặc email dinhdongphuong@gmail.com để nắm rõ và đăng ký tham gia bạn nhé.
             </td>
-          </tr>
-          <tr>
-            <td>Row 2, Column 1</td>
-            <td>Row 2, Column 2</td>
-            <td>Row 2, Column 3</td>
           </tr>
         </tbody>
       </table>
